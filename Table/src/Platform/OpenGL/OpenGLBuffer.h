eccfeccdb7108a7f841fbb2355351001a8e271cc
liangliang24 <2794515623@qq.com>
2024-04-07 17:48:16 +0800

    Add Buffer Layout

--- Table/src/Platform/OpenGL/OpenGLBuffer.h
+++ Table/src/Platform/OpenGL/OpenGLBuffer.h
@@ -13,8 +13,11 @@ namespace Table
 		virtual void Bind() const;
 		virtual void UnBind() const;
 
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer
