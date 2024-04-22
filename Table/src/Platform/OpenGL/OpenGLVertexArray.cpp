1781319c57c91ae69b355fa9049514c49b97d4ae
liangliang24 <2794515623@qq.com>
2024-04-22 16:33:51 +0800

    fix type cast warning

--- Table/src/Platform/OpenGL/OpenGLVertexArray.cpp
+++ Table/src/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -76,7 +76,7 @@ namespace Table
 				ShaderDataTypeToOpenGLBaseType(element.Type),
 				element.Normalized ? GL_TRUE : GL_FALSE,
 				layout.GetStride(),
-				(const void*)(uintptr_t)element.Offset
+				(const void*)element.Offset
 			);
 			m_VertexBufferIndex++;
 		}
--- Table/src/Table/Renderer/Buffer.h
+++ Table/src/Table/Renderer/Buffer.h
@@ -46,10 +46,10 @@ namespace Table
 		std::string Name;
 		ShaderDataType Type;
 		uint32_t Size;
-		uint32_t Offset;
+		size_t Offset;
 		bool Normalized;
 
-		BufferElement() {}
+		BufferElement() = default;
 
 		BufferElement(ShaderDataType type, const std::string& name, bool normalized = false)
 			:Name(name), Type(type), Size(ShaderDataTypeSize(type)), Offset(0), Normalized(normalized)
@@ -113,7 +113,7 @@ namespace Table
 	private:
 		void CalculateOffsetsAndStride()
 		{
-			uint32_t offset = 0;
+			size_t offset = 0;
 			m_Stride = 0;
 			for (auto& element : m_Elements)
 			{
