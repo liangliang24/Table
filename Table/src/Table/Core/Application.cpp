ad277145f94f23169b17806160c90a0b2a9c9d03
liangliang24 <2794515623@qq.com>
2024-04-23 17:19:27 +0800

    Wrap iterator  in LayerStack

--- Table/src/Table/Core/Application.cpp
+++ Table/src/Table/Core/Application.cpp
@@ -90,9 +90,9 @@ namespace Table
 		dispatcher.Dispatch<WindowCloseEvent>(TABLE_BIND_EVENT_FN(Application::OnWindowClose));
 		dispatcher.Dispatch<WindowResizeEvent>(TABLE_BIND_EVENT_FN(Application::OnWindowResize));
 		
-		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin();)
+		for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend();  ++it)
 		{
-			(*--it)->OnEvent(e);
+			(*it)->OnEvent(e);
 			if (e.Handled)
 			{
 				break;
