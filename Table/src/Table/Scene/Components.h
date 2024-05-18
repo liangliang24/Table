dcda4b84c1a3ff5c8235c84c8d8d61886dfa6d37
LLTouchingFish <62233176+liangliang24@users.noreply.github.com>
2024-05-18 12:55:19 +0800

    Add Texture Drag/Drop

--- Table/src/Table/Scene/Components.h
+++ Table/src/Table/Scene/Components.h
@@ -8,6 +8,7 @@
 
 #include "Table/Scene/SceneCamera.h"
 #include "Table/Scene/ScriptableEntity.h"
+#include "Table/Renderer/Texture.h"
 
 
 namespace Table
@@ -54,6 +55,9 @@ namespace Table
 	struct SpriteRendererComponent
 	{
 		glm::vec4 Color{ 1.0f,1.0f,1.0f,1.0f };
+		Ref<Texture2D> Texture;
+		float TilingFactor = 1.0f;
+
 		SpriteRendererComponent() = default;
 		SpriteRendererComponent(const SpriteRendererComponent&) = default;
 		SpriteRendererComponent(const glm::vec4& color)
